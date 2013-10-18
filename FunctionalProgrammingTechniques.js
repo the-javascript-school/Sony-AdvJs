613002a98463bbe5a8495d7227b21ebb1856aa87
the-javascript-school <tkmagesh@craftsmanfactory.com>
2013-10-18 11:27:57 -0700

    Day - 2 Timer Functions

--- FunctionalProgrammingTechniques.js
+++ FunctionalProgrammingTechniques.js
@@ -15,3 +15,38 @@ function sort(list,criteria){
          }
    return list;
 }
+function map(list,operationFn){
+  for(var i=0;i<list.length;i++){
+     operationFn(list[i]);
+  }
+}
+
+function bind(fn, thisContext){
+  var _fn = fn,
+  	  _thisContext = thisContext;
+  return function(){
+      return _fn.apply(_thisContext,Array.prototype.splice.call(arguments,0));
+  }
+}
+
+
+function f(){ console.log(this.name);}
+
+var a = { name : "a"};
+var b = { name : "b"};
+
+a.m = f;
+a.m() // prints "a"
+
+b.m = f;
+b.m() //prints "b"
+
+var refObj = {name : "refObj"};
+
+f = bind(f,refObj); //implement the "bind" function
+
+a.m = f;
+a.m() // should print "refObj"
+
+b.m = f;
+b.m() // should print "refObj"
